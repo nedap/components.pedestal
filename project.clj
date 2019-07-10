3f6bf9f98080ac0c1136f8494216e71554994160
vemv <vemv@users.noreply.github.com>
2019-07-10 02:53:01 +0200

    Setup CircleCI, ci.release-workflow

--- project.clj
+++ project.clj
@@ -1,14 +1,19 @@
 (defproject com.nedap.staffing-solutions/components.pedestal "0.4.1"
+  ;; Please don't bump the library version by hand - use ci.release-workflow instead.
   ;; Please keep the dependencies sorted a-z.
-  :dependencies [[ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [com.grzm/component.pedestal "0.1.7"]
+  :dependencies [[ch.qos.logback/logback-classic "1.2.3"
+                  :exclusions [org.slf4j/slf4j-api]]
+                 [com.grzm/component.pedestal "0.1.7"
+                  :exclusions [ring/ring-codec]]
                  [com.nedap.staffing-solutions/utils.modular "0.4.0"]
-                 [com.nedap.staffing-solutions/utils.spec "0.8.2"]
+                 [com.nedap.staffing-solutions/utils.spec "0.8.2"
+                  :exclusions [org.clojure/spec.alpha]]
                  [com.stuartsierra/component "0.4.0"]
                  [io.pedestal/pedestal.jetty "0.5.5"]
                  [io.pedestal/pedestal.service "0.5.5"]
                  [medley "1.1.0"]
                  [org.clojure/clojure "1.10.1"]
+                 [org.clojure/spec.alpha "0.2.176"]
                  [org.slf4j/jcl-over-slf4j "1.7.26"]
                  [org.slf4j/jul-to-slf4j "1.7.26"]
                  [org.slf4j/log4j-over-slf4j "1.7.26"]]
@@ -40,15 +45,33 @@
 
   :repl-options {:init-ns dev}
 
+  ;; A variety of common dependencies are bundled with `nedap/lein-template`.
+  ;; They are divided into two categories:
+  ;; * Dependencies that are possible or likely to be needed in all kind of production projects
+  ;;   * The point is that when you realise you needed them, they are already in your classpath, avoiding interrupting your flow
+  ;;   * After realising this, please move the dependency up to the top level.
+  ;; * Genuinely dev-only dependencies allowing 'basic science'
+  ;;   * e.g. criterium, deep-diff, clj-java-decompiler
 
+  ;; NOTE: deps marked with #_"transitive" are there to satisfy the `:pedantic?` option.
   :profiles {:dev {:dependencies [[cider/cider-nrepl "0.16.0" #_"formatting-stack needs it"]
                                   [com.clojure-goes-fast/clj-java-decompiler "0.2.1"]
                                   [com.taoensso/timbre "4.10.0"]
                                   [criterium "0.4.4"]
-                                  [formatting-stack "0.18.4"]
+                                  [formatting-stack "0.18.4"
+                                   :exclusions [rewrite-clj]]
                                   [lambdaisland/deep-diff "0.0-29"]
+                                  [medley "1.1.0"]
+                                  [org.clojure/core.async "0.4.490"]
+                                  [org.clojure/math.combinatorics "0.1.1"]
                                   [org.clojure/test.check "0.10.0-alpha3"]
                                   [org.clojure/tools.namespace "0.3.0-alpha4"]
-                                  [org.clojure/tools.reader "1.1.1" #_"formatting-stack needs it"]]
+                                  [org.clojure/tools.reader "1.1.1" #_"transitive"]
+                                  [rewrite-clj "0.6.1" #_"transitive"]]
                    :plugins      [[lein-cloverage "1.0.13"]]
-                   :source-paths ["dev" "test"]}})
+                   :source-paths ["dev" "test"]}
+
+             :ci  {:pedantic?    :abort
+                   :jvm-opts     ["-Dclojure.main.report=stderr"]
+                   :global-vars  {*assert* true} ;; `ci.release-workflow` relies on runtime assertions
+                   :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.1.0-alpha6"]]}})
