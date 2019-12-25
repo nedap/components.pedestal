10ed879acf7e70a21772d2772b09bf69dcd3fead
vemv <vemv@users.noreply.github.com>
2019-12-25 23:43:59 +0100

    Reapply our Lein template

--- project.clj
+++ project.clj
@@ -23,9 +23,6 @@
 
   :min-lein-version "2.0.0"
 
-  :license {:name "EPL-2.0"
-            :url  "https://www.eclipse.org/legal/epl-2.0/"}
-
   :signing {:gpg-key "releases-staffingsolutions@nedap.com"}
 
   :repositories {"releases" {:url      "https://nedap.jfrog.io/nedap/staffing-solutions/"
@@ -52,26 +49,31 @@
   ;;   * e.g. criterium, deep-diff, clj-java-decompiler
 
   ;; NOTE: deps marked with #_"transitive" are there to satisfy the `:pedantic?` option.
-  :profiles {:dev  {:dependencies [[cider/cider-nrepl "0.16.0" #_"formatting-stack needs it"]
-                                   [com.clojure-goes-fast/clj-java-decompiler "0.2.1"]
-                                   [com.nedap.staffing-solutions/utils.spec.predicates "1.1.0"]
-                                   [com.taoensso/timbre "4.10.0"]
-                                   [criterium "0.4.4"]
-                                   [formatting-stack "1.0.0"]
-                                   [lambdaisland/deep-diff "0.0-29"]
-                                   [medley "1.2.0"]
-                                   [org.clojure/core.async "0.5.527"]
-                                   [org.clojure/math.combinatorics "0.1.1"]
-                                   [org.clojure/test.check "0.10.0-alpha3"]
-                                   [org.clojure/tools.namespace "0.3.0-alpha4"]]
-                    :plugins      [[lein-cloverage "1.1.1"]]
-                    :source-paths ["dev"]
-                    :repl-options {:init-ns dev}}
+  :profiles {:dev   {:dependencies [[cider/cider-nrepl "0.16.0" #_"formatting-stack needs it"]
+                                    [com.clojure-goes-fast/clj-java-decompiler "0.2.1"]
+                                    [com.nedap.staffing-solutions/utils.spec.predicates "1.1.0"]
+                                    [com.taoensso/timbre "4.10.0"]
+                                    [criterium "0.4.5"]
+                                    [formatting-stack "1.0.1"]
+                                    [lambdaisland/deep-diff "0.0-29"]
+                                    [medley "1.2.0"]
+                                    [org.clojure/core.async "0.5.527"]
+                                    [org.clojure/math.combinatorics "0.1.1"]
+                                    [org.clojure/test.check "0.10.0-alpha3"]
+                                    [org.clojure/tools.namespace "0.3.1"]]
+                     :plugins      [[lein-cloverage "1.1.1"]]
+                     :source-paths ["dev"]
+                     :repl-options {:init-ns dev}}
 
-             :test {:dependencies [[com.nedap.staffing-solutions/utils.test "1.6.1"]]
-                    :jvm-opts     ["-Dclojure.core.async.go-checking=true"]}
+             :check {:global-vars {*unchecked-math* :warn-on-boxed
+                                   ;; avoid warnings that cannot affect production:
+                                   *assert*         false}}
 
-             :ci   {:pedantic?    :abort
-                    :jvm-opts     ["-Dclojure.main.report=stderr"]
-                    :global-vars  {*assert* true} ;; `ci.release-workflow` relies on runtime assertions
-                    :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.6.0"]]}})
+             :test  {:dependencies [[com.nedap.staffing-solutions/utils.test "1.6.1"]]
+                     :jvm-opts     ["-Dclojure.core.async.go-checking=true"
+                                    "-Duser.language=en-US"]}
+
+             :ci    {:pedantic?    :abort
+                     :jvm-opts     ["-Dclojure.main.report=stderr"]
+                     :global-vars  {*assert* true} ;; `ci.release-workflow` relies on runtime assertions
+                     :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.6.0"]]}})
